65bc4a1f77b3814a9edd2ba5fd28c9bb7354bfa4
Alexander Bauer <sasha@linux.com>
2017-02-27 23:21:32 -0500

    Make version numbers informative and standard

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
+import re
 import subprocess
 
 try:
-    version = subprocess.check_output(
-            ['git', 'describe', '--always', '--dirty=-plus']) \
-            .decode('UTF-8').strip()
+    description = subprocess.check_output(
+        ['git', 'describe', '--always', '--dirty=.dirty']) \
+        .decode('UTF-8').strip()
+    version_data = re.match('''
+            (?P<release>[0-9]+(\.[0-9]+)*) # Match the release version
+            (
+                -(?P<dev>[0-9]+) # Match a since-last-tag number if present
+            )?
+            (
+                -g
+                (?P<git>[0-9a-f]+)   # Match the commit ID if present
+                (?P<dirty>\.dirty)?  # Match the "dirty" identifier if present
+            )
+            ''',
+            description, re.VERBOSE)
+    groups = {group: (value if value else '')
+            for group, value
+            in version_data.groupdict().items()}
+    version = '{release}{dev_segment}{internal_segment}'.format(
+        dev_segment='.dev'+groups['dev'] if groups['dev'] else '',
+        internal_segment='+'+groups['git']+groups['dirty'] if groups['git'] else '',
+        **version_data.groupdict())
+
 except Exception as e:
     print("Could not get version: {}", repr(e))
     version = None
