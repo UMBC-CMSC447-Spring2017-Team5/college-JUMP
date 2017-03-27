62f06b08e567029fd893ca7e6e30c7a3cdf8ab1b
Mjacks3 <mjacks3@umbc.edu>
2017-03-27 00:19:07 -0400

    Added Assignment Pages + demo submission variations
    So I couldn't get the javascript working to do a sidebar/vertical nav.
    If anyone wants to do that, knock yourself out. Or if we want to leave
    it as is, that's fine too.

--- collegejump/views.py
+++ collegejump/views.py
@@ -22,3 +22,28 @@ def login_page():
     return flask.render_template('login.html',
                                  __version__=app.config["VERSION"],
                                  gcal_link="dummylink")
+@app.route('/weekone.html')
+def weekone_page():
+    return flask.render_template('weekone.html',
+                                 __version__=app.config["VERSION"],
+                                 gcal_link="dummylink")
+@app.route('/weektwo.html')
+def weektwo_page():
+    return flask.render_template('weektwo.html',
+                                 __version__=app.config["VERSION"],
+                                 gcal_link="dummylink")
+@app.route('/weekthree.html')
+def weekthree_page():
+    return flask.render_template('weekthree.html',
+                                 __version__=app.config["VERSION"],
+                                 gcal_link="dummylink")
+@app.route('/weekfour.html')
+def weekfour_page():
+    return flask.render_template('weekfour.html',
+                                 __version__=app.config["VERSION"],
+                                 gcal_link="dummylink")
+@app.route('/weekfive.html')
+def weekfive_page():
+    return flask.render_template('weekfive.html',
+                                 __version__=app.config["VERSION"],
+                                 gcal_link="dummylink")
