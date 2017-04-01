c80795d95d31582967892980611e158d87b8174e
Brad <ichniow1@umbc.edu>
2017-04-01 19:34:38 -0400

    Add version to footer

--- collegejump/__init__.py
+++ collegejump/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
+import subprocess, string
 
-__version__ = None
-try:
-    from collegejump._version import __version__
-except ImportError:
-    print("WARN: no _version.py; is the package installed??")
-    __version__ = "development"
+def get_git_revision_hash():
+    try:
+        s = str(subprocess.check_output(["git", "describe", "--always"]).strip())
+        # return format is like b'versionstring'\n
+        # so partition between apostrophe character
+        s = s.partition('\'')[-1].rpartition('\'')[0]
+        return s
+    except:
+        return "Subprocess Exception"
+
+#try:
+#    from collegejump._version import __version__
+#except ImportError:
+#    print("WARN: no _version.py; is the package installed??")
+__version__ = "College JUMP version " + get_git_revision_hash()
+
 
 app = Flask(__name__)
 
