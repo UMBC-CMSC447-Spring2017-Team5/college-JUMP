52f34b5307b79c6a585a45e2c046db55c9d6cbb6
Alexander Bauer <sasha@linux.com>
2017-04-02 17:26:32 -0400

    Draft out most of the database modelling

--- collegejump/models.py
+++ collegejump/models.py
@@ -8,12 +8,36 @@ db = app.db
 
 bcrypt = Bcrypt(app)
 
+week_assignments = db.Table('week_assignments', db.metadata,
+        db.Column('semester_id', db.Integer),
+        db.Column('week_number', db.Integer),
+        db.Column('assignment_id', db.Integer, db.ForeignKey('assignment.id')),
+        db.ForeignKeyConstraint(
+            ['semester_id', 'week_number'],
+            ['week.semester_id', 'week.week_number']
+        ))
+
+week_documents = db.Table('week_documents', db.metadata,
+        db.Column('semester_id', db.Integer),
+        db.Column('week_number', db.Integer),
+        db.Column('document_id', db.Integer, db.ForeignKey('document.id')),
+        db.ForeignKeyConstraint(
+            ['semester_id', 'week_number'],
+            ['week.semester_id', 'week.week_number']
+        ))
 class User(db.Model, UserMixin):
+    NAME_MAX_LENGTH = 128
     EMAIL_MAX_LENGTH = 128
 
     id        = db.Column(db.Integer, primary_key=True)
+    name      = db.Column(db.String(NAME_MAX_LENGTH))
     email     = db.Column(db.String(EMAIL_MAX_LENGTH), unique=True)
     _password = db.Column(db.String(128)) # Bcrypt string, not plaintext
+
+    admin     = db.Column(db.Boolean)
+
+    mentor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    mentees   = db.relationship('User', backref=db.backref('mentor', remote_side=[id]))
 
     def __init__(self, email, plaintext):
         self.email = email
@@ -44,3 +68,42 @@ class User(db.Model, UserMixin):
     @app.login_manager.user_loader
     def load_user(user_email):
         return User.query.filter_by(email=user_email).first()
+
+class Announcement(db.Model):
+    TITLE_MAX_LENGTH = 128
+    CONTENT_MAX_LENGTH = 1000
+
+    id        = db.Column(db.Integer, primary_key=True)
+    title     = db.Column(db.String(TITLE_MAX_LENGTH))
+    content   = db.Column(db.String(CONTENT_MAX_LENGTH))
+    timestamp = db.Column(db.DateTime())
+
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author    = db.relationship('User')
+
+class Semester(db.Model):
+    """A semester is a collection of weeks meant to make up the content of the
+    website during a whole semester."""
+
+    id    = db.Column(db.Integer, primary_key=True)
+    start = db.Column(db.Date())
+    end   = db.Column(db.Date())
+
+class Week(db.Model):
+    # Weeks are identified as (semester, week number) uniquely.
+    semester_id = db.Column(db.Integer, db.ForeignKey('semester.id'), primary_key=True)
+    week_number = db.Column(db.Integer, primary_key=True)
+
+    semester = db.relationship('Semester',
+            backref=db.backref('weeks', order_by=week_number))
+
+    assignments = db.relationship('Assignment', secondary=week_assignments)
+    documents   = db.relationship('Document',   secondary=week_documents)
+
+class Assignment(db.Model):
+    id        = db.Column(db.Integer, primary_key=True)
+    questions = db.Column(db.Text) # a JSON blob
+
+class Document(db.Model):
+    id   = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.LargeBinary)
