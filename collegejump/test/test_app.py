c2c06b6773a0383ee754c2a41df28942d89795f6
kongmingtwo <ichniow1@umbc.edu>
2017-04-29 21:26:59 -0400

    still fail but... comments?

--- collegejump/test/test_app.py
+++ collegejump/test/test_app.py
@@ -25,22 +25,35 @@ class TestCollegeJUMP():
         rv = app.get('/')
         assert bytes(version, encoding='UTF-8') in rv.data
     
+    '''
+    Test the main url, which seems to have an HTTP response code of
+    301 during normal operation
+    '''
     def test_http_index(self, collegejump, app):
         base_url = 'http://localhost:8088'
-        d = open(debug_f, 'w')
+        d = open(debug_f, 'a')
         x = app.get(base_url)
         d.write("test_http_index\n")
         d.write(str(x))
         d.close()
         assert(x.status_code == 301)
+
+    '''
+    Test the rest of the urls via HTTP response codes.
+    Errs out when one of the response codes isn't 200 (OK)
+    For debug info check file 'debug_out' off the main directory.
+    '''
     def test_http_site(self, collegejump, app):
-        d = open(debug_f, 'w')
+        d = open(debug_f, 'a')
         d.write("test_http_site\n")
+        #add urls here as needed
         url_list = ['announcement/', 'calendar']
         base_url = 'http://localhost:8088/'
         for u in url_list:
             d.write('\t' + base_url + u + '\n')
             x = app.get(base_url + u + '\n')
             d.write('\t' + str(x) + '\n')
+            if(x.status_code != 200):
+                d.write("test broke on url=" + u)
             assert(x.status_code == 200)     
         d.close()
